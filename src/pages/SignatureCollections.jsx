0b16377b662c558201c268975826fe8b05236877
chaman bhalnai <chamanbhalnai26@gmail.com>
2025-02-26 16:35:48 +0530

    latest

--- src/pages/SignatureCollections.jsx
+++ src/pages/SignatureCollections.jsx
@@ -5,6 +5,7 @@ import Logo from "../components/Logo";
 import Navbar from "../components/Navbar";
 import Collection from "../components/Collection";
 import Card from "../components/Cards";
+import Footer from "../components/Footer";
 
 import {
   logoData,
@@ -22,6 +23,7 @@ const SignatureCollections = () => {
   const firstUrl = getUrl(signatureCollectionImgData, 0, "imgUrl");
   const secondUrl = getUrl(signatureCollectionImgData, 1, "imgUrl");
   const thirdUrl = getUrl(signatureCollectionImgData, 2, "imgUrl");
+  const fourthUrl = getUrl(signatureCollectionImgData, 3, "imgUrl");
 
   const filterImageByCategory = (data, category) => {
     return data.filter((item) => item.category === category);
@@ -33,6 +35,14 @@ const SignatureCollections = () => {
   const mlovImages = filterImageByCategory(
     secretGardenCardData,
     "olivetta-card-collection"
+  );
+  const mlcmImages = filterImageByCategory(
+    secretGardenCardData,
+    "camellia-card-collection"
+  );
+  const mteImages = filterImageByCategory(
+    secretGardenCardData,
+    "mte-signature"
   );
 
   const renderCards = (imageArray) => {
@@ -66,8 +76,8 @@ const SignatureCollections = () => {
 
         <div>
           <img src={secondUrl} alt="abcd" className="secret-garden-photo1" />
-          <h2>Proportion & Scale</h2>
-          <p>
+          <h2 className="collection-head">Proportion & Scale</h2>
+          <p className="collection-para">
             Taking inspiration from the asymmetry of nature and its lush
             foliage, we based Secret Garden on blooms, buds, and intertwining
             vines.
@@ -85,10 +95,10 @@ const SignatureCollections = () => {
         <h1 className="olivetta-collection-head">
           The Secret Garden Collection
         </h1>
-        <Collection data={shopByData[14]} />
-        <img src={secondUrl} alt="abcd" className="the-olivetta-photo1" />
-        <h2>Timeless & Evocative</h2>
-        <p>
+        <Collection data={shopByData[14]} className="reverse-sngr"/>
+        <img src={thirdUrl} alt="abcd" className="the-olivetta-photo1" />
+        <h2 className="collection-head">Timeless & Evocative</h2>
+        <p className="collection-para">
           Capturing the elegant angles of the Mediterranean olive tree, Olivetta
           echoes the light that weaves through branches and sculpted, slender
           leaves
@@ -98,6 +108,33 @@ const SignatureCollections = () => {
           <div className="mlov-cards">{renderCards(mlovImages)}</div>
         </div>
       </div>
+
+      <div>
+        <div className="camellia-collection">
+          <h1 className="camellia-collection-head">The Camellia Collection</h1>
+          <Collection data={shopByData[15]} />
+
+          <img src={fourthUrl} alt="abcd" className="the-camellia-photo1" />
+
+          <h2 className="collection-head">Balance & Beauty</h2>
+          <p className="collection-para">
+            Inspired by their namesake flower, Camellia designs feature textural
+            details, unique petal motifs, and delicate diamond leav
+          </p>
+        </div>
+
+        <div className="most-loved-camellia">
+          <h2 className="mlcm">Most Loved Camellia Styles</h2>
+          <div className="mlcm-cards">{renderCards(mlcmImages)}</div>
+        </div>
+      </div>
+
+      <div className="more-to-exp-signature">
+        <h1 className="mte-signature-head">More to Explore</h1>
+        <div className="mte-signature-cards">{renderCards(mteImages)}</div>
+      </div>
+
+      <Footer />
     </>
   );
 };
