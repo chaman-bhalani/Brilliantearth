9524bd79e0911696d3bc715d77699bb85b383a4e
chaman bhalnai <108402674+chaman-bhalani@users.noreply.github.com>
2025-02-28 10:38:07 +0530

    added css

--- src/pages/WeveGotYouCovered.jsx
+++ src/pages/WeveGotYouCovered.jsx
@@ -5,12 +5,15 @@ import Navbar from "../components/Navbar";
 import Collection from "../components/Collection";
 import CollectionCards from "../components/CollectionCards";
 import Footer from "../components/Footer";
+import Card from "../components/Cards";
+import BackgroundImage from "../components/PosterPhoto";
 
 import {
   logoData,
   shopByData,
   appointmentData,
   unmatchedCareData,
+  bgImgGftData,
 } from "../assets/data";
 
 const WeveGotYouCovered = () => {
@@ -21,6 +24,33 @@ const WeveGotYouCovered = () => {
   const logoUrl = getUrl(logoData, 0, "logoUrl");
 
   const dcUrl = getUrl(appointmentData, 1, "imgUrl");
+
+  const filterImageByCategory = (data, category) => {
+    return data.filter((item) => item.category === category);
+  };
+
+  const unmatchedCare = filterImageByCategory(
+    unmatchedCareData,
+    "unmatched-care"
+  );
+  const shopByWGC = filterImageByCategory(unmatchedCareData, "wgc-shopby");
+
+  const backgroundData = Array.isArray(bgImgGftData)
+    ? bgImgGftData[8]
+    : bgImgGftData;
+
+  const renderCards = (imageArray) => {
+    return imageArray.map((item) => (
+      <Card
+        key={item.id}
+        url={item.imgUrl}
+        alt={item.alt}
+        footer={item.footer}
+        hoverUrl={item.hoverUrl}
+      />
+    ));
+  };
+
   return (
     <>
       <div>
@@ -39,7 +69,7 @@ const WeveGotYouCovered = () => {
       <div className="unmatched-care">
         <h1 className="unmatched-care-head">Our Unmatched Care</h1>
         <CollectionCards
-          data={unmatchedCareData}
+          data={unmatchedCare}
           containerClass="jwl-collections"
           cardClass="jwl-collections-cards"
           imageClass="jwl-collections-img"
@@ -50,6 +80,19 @@ const WeveGotYouCovered = () => {
 
       <Collection data={shopByData[39]} />
       <Collection data={shopByData[40]} className="rare-gemstone" />
+
+      <BackgroundImage
+        imageSrc={backgroundData.imgUrl}
+        heading={backgroundData.heading}
+        paragraph={backgroundData.paragraph}
+        buttons={backgroundData.buttons}
+        customClass="personal-attention"
+      />
+
+      <div className="wgc-shopby-container">
+        <h1 className="wgc-shopby-head">Shop By Category</h1>
+        <div className="wgc-shopby-cards">{renderCards(shopByWGC)}</div>
+      </div>
 
       <Footer />
     </>
