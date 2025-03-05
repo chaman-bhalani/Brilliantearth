935036da2ed7c0cb7162b0d87b23a2bbcdd1bb99
chaman bhalnai <108402674+chaman-bhalani@users.noreply.github.com>
2025-03-05 11:13:59 +0530

    completed page

--- src/pages/EngagementRingTrends.jsx
+++ src/pages/EngagementRingTrends.jsx
@@ -5,12 +5,15 @@ import Navbar from "../components/Navbar";
 import BackgroundImage from "../components/PosterPhoto";
 import Collection from "../components/Collection";
 import CollectionWithTopPara from "../components/CollectionWithTopPara";
+import Card from "../components/Cards";
+import Footer from "../components/Footer";
 
 import {
   logoData,
   bgImgGftData,
   WeddingRingTrendsData,
   topEightRingsData,
+  topEngRingsCardData,
 } from "../assets/data";
 
 const EngagementRingTrends = () => {
@@ -28,9 +31,14 @@ const EngagementRingTrends = () => {
     return data.filter((item) => item.category === category);
   };
 
-  const clOneImages = filterImageByCategory(topEightRingsData, "cl-one");
-
-
+  const clOneImages = filterImageByCategory(topEngRingsCardData, "cl-one");
+  const clTwoImages = filterImageByCategory(topEngRingsCardData, "cl-two");
+  const clThreeImages = filterImageByCategory(topEngRingsCardData, "cl-three");
+  const clFourImages = filterImageByCategory(topEngRingsCardData, "cl-four");
+  const clFiveImages = filterImageByCategory(topEngRingsCardData, "cl-five");
+  const clSixImages = filterImageByCategory(topEngRingsCardData, "cl-six");
+  const clSevenImages = filterImageByCategory(topEngRingsCardData, "cl-seven");
+  const clEightImages = filterImageByCategory(topEngRingsCardData, "cl-eight");
 
   const renderCards = (imageArray) => {
     return imageArray.map((item) => (
@@ -43,9 +51,20 @@ const EngagementRingTrends = () => {
       />
     ));
   };
+
+  const cardSets = [
+    clOneImages,
+    clTwoImages,
+    clThreeImages,
+    clFourImages,
+    clFiveImages,
+    clSixImages,
+    clSevenImages,
+    clEightImages,
+  ];
   return (
     <>
-      <Logo logoSrc={logoUrl} altText="" />
+      <Logo logoSrc={logoUrl} altText="Logo" />
       <Navbar />
 
       <BackgroundImage
@@ -59,30 +78,36 @@ const EngagementRingTrends = () => {
       <Collection data={WeddingRingTrendsData[0]} className="enr-cl-reverse" />
 
       <div className="top-eight-en-rings">
-        <h1 className="top-eight-en-rings-head">
-          Top 8 Engagement Ring Trends of 2025
-        </h1>
-        <p className="top-eight-en-rings-para">
-          In 2025, trends will continue to embrace bold expressions of
-          individuality, perfectly mirroring the distinctive engagement ring
-          styles that are gaining in popularity. From the resurgence of
-          vintage-inspired halo settings that add a touch of romance to sleek
-          and contemporary solitaire rings that speak to modern sophistication,
-          there's a trend to match every aesthetic.
-        </p>
-
-        <div className="first-cl-cards">{renderCards(clOneImages)}</div>
+        <div className="top-eight-en-rings-text">
+          <h1 className="top-eight-en-rings-head">
+            Top 8 Engagement Ring Trends of 2025
+          </h1>
+          <p className="top-eight-en-rings-para">
+            In 2025, trends will continue to embrace bold expressions of
+            individuality, perfectly mirroring the distinctive engagement ring
+            styles that are gaining in popularity. From the resurgence of
+            vintage-inspired halo settings that add a touch of romance to sleek
+            and contemporary solitaire rings that speak to modern
+            sophistication, there's a trend to match every aesthetic.
+          </p>
+        </div>
 
         {topEightRingsData.map((data, index) => (
-          <CollectionWithTopPara
-            key={index}
-            data={data}
-            className={
-              index % 2 === 1 ? "reverse-eng-rings-cl" : "eng-rings-cl"
-            }
-          />
+          <React.Fragment key={index}>
+            <CollectionWithTopPara
+              data={data}
+              className={
+                index % 2 === 1 ? "reverse-eng-rings-cl" : "eng-rings-cl"
+              }
+            />
+            <div className="top-eight-enr-cards">
+              {renderCards(cardSets[index])}
+            </div>
+          </React.Fragment>
         ))}
       </div>
+
+      <Footer />
     </>
   );
 };
